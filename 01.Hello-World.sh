635db1949a00cf358c0844a1ae0a04ed067518ec
Rufus Burugu <rufus_b@hotmail.com>
2026-03-10 11:57:08 +0530

    New line added

--- 01.Hello-World.sh
+++ 01.Hello-World.sh
@@ -2,3 +2,4 @@
 # This is a simple script to print "Hello, World!" to the console.
 echo "Hello, World!"
 echo "I am teaching Shell Scripting to my Friends."
+echo "this is another update to test the commit and push process."
