ed8ef2ebd66cd85e806aa5b3ce8f72941b458ad1
Rufus Burugu <rufus_b@hotmail.com>
2026-03-16 22:14:10 +0530

    just another commit

--- 01.Hello-World.sh
+++ 01.Hello-World.sh
@@ -4,3 +4,10 @@ echo "Hello, World!"
 echo "this is another update to test the commit and push process."
 echo "One last update to test the commit and push process."
 echo "Burn it down"
+
+# another echo statements to test the commit and push process.
+echo "Hi Rufus, How are you doing today?"
+echo "Hey, wassup Jude, I am doing well, how are you doing?"
+echo "I am doing great, thanks for asking. I am looking forward to our next meeting"
+echo "Yeah, me too. Lets meet up nex week"
+
