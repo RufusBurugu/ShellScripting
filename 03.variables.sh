d02ce089966ecb850eb210f87a85da0ea272df4d
Rufus Burugu <rufus_b@hotmail.com>
2026-03-16 23:50:23 +0530

    nithing much

---
+++ 03.variables.sh
@@ -0,0 +1,9 @@
+#!/bin/bash
+
+PERSON1=Rufus
+PERSON2=Jude
+
+echo "$PERSON1:: Hi $PERSON2, How are you doing today?"
+echo "$PERSON2:: Hey, wassup $PERSON1, I am doing well, how are you doing?"
+echo "$PERSON1:: I am doing great, thanks for asking. I am looking forward to our next meeting"
+echo "$PERSON2:: Yeah, me too. Lets meet up nex week"
